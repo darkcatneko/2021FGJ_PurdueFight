be85477c2af0f9f1d9bf4a646e84be280452d192
darkcatneko <darkcat7126@gmail.com>
2021-08-28 19:51:02 +0800

    present version

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     [System.Flags]
     enum Dir 
     {
@@ -14,9 +15,9 @@ public class Player : MonoBehaviour
 
     }
 
-        Dir dir = (Dir.Up | Dir.Left);
-        int PlayerID;
-        int TableCount;
+    Dir dir = (Dir.Up | Dir.Left);
+        public int PlayerID;
+        public int TableCount;
         bool Ifgethit;
         bool IfEating;
         float Hunger;
@@ -25,51 +26,64 @@ public class Player : MonoBehaviour
         {
         PlayerID = Id;
         }
-        public void ChangeTable()
+    public void ChangeTable()
+    {
+        //Debug.Log((int)dir);
+        if (InputButtonDown.Up(PlayerID))
         {
-        Debug.Log((int)dir);
-            if (InputButtonDown.Up(PlayerID))
-            {
             dir = (dir | Dir.Up) ^ Dir.Down;
-            }
-            else if (InputButtonDown.Down(PlayerID))
-            {
-            dir = (dir | Dir.Down) ^ Dir.Up;
-            }
-            if (InputButtonDown.Left(PlayerID))
-            {
-            dir = (dir | Dir.Left) ^ Dir.Right;
-            }
-            else if (InputButtonDown.Right(PlayerID))
-            {
-            dir = (dir | Dir.Right) ^ Dir.Left;
-            }
-            
-            if (Dir.Up == (dir & Dir.Up))
-            {
-            transform.SetLocalPositioinY(3.5f);
-            }
-            else
-            {
-            transform.SetLocalPositioinY(-1.2f);
-            }
-            if (Dir.Right == (dir & Dir.Right))
-            {
-            transform.SetLocalPositioinX(4.2f);
-            }
-            else
-            {
-            transform.SetLocalPositioinX(-4.2f);
-            }
-        Debug.Log((int)dir);
-    }
-        public void EatFood()
-        {
-
         }
+        else if (InputButtonDown.Down(PlayerID))
+        {
+            dir = (dir | Dir.Down) ^ Dir.Up;
+        }
+        if (InputButtonDown.Left(PlayerID))
+        {
+            dir = (dir | Dir.Left) ^ Dir.Right;
+        }
+        else if (InputButtonDown.Right(PlayerID))
+        {
+            dir = (dir | Dir.Right) ^ Dir.Left;
+        }
+
+        if (Dir.Up == (dir & Dir.Up))
+        {
+            transform.SetLocalPositioinY(3.5f);
+        }
+        else
+        {
+            transform.SetLocalPositioinY(-1.2f);
+        }
+        if (Dir.Right == (dir & Dir.Right))
+        {
+            transform.SetLocalPositioinX(4.2f);
+        }
+        else
+        {
+            transform.SetLocalPositioinX(-4.2f);
+        }
+        //Debug.Log((int)dir);
+    }
+
+    void EatFood()
+    {
+        GameObject[] myObjArray;
+        myObjArray = GameObject.FindGameObjectsWithTag("Food");
+        if (InputButtonDown.Act(PlayerID))
+        {
+            foreach (var item in myObjArray)
+            {
+                if ((int)dir == item.GetComponent<Food>().tableID)
+                {
+                    item.GetComponent<Food>().BeenEaten();
+                }
+            }
+        }
+    }
     private void Update()
     {
         ChangeTable();
+        EatFood();
     }
 }
 
