4fd92c0e9c9cf6447898d7b586e70a8929a7687c
darkcatneko <darkcat7126@gmail.com>
2021-08-29 12:41:32 +0800

    sprite on

--- Assets/Script/Food.cs
+++ Assets/Script/Food.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    [SerializeField] GameObject pic10;
+    [SerializeField] GameObject pic9;[SerializeField] GameObject pic8;[SerializeField] GameObject pic7;
+    [SerializeField] GameObject pic6;[SerializeField] GameObject pic5;[SerializeField] GameObject pic4;
     [SerializeField] GameObject pic3;[SerializeField] GameObject pic2;[SerializeField] GameObject pic1;
     public int tableID;
-    public float FoodCount = 60;
+    public float FoodCount = 70;
     public void SetID(int id)
     {
         tableID = id;
@@ -20,13 +23,41 @@ public class Food : MonoBehaviour
         
         if (FoodCount<0)
         {
+            pic10.SetActive(false);
+        }
+        else if (FoodCount<8)
+        {
+            pic9.SetActive(false);
+        }
+        else if (FoodCount<15)
+        {
+            pic8.SetActive(false);
+        }
+        else if (FoodCount < 22)
+        {
+            pic7.SetActive(false);
+        }
+        else if (FoodCount < 29)
+        {
+            pic6.SetActive(false);
+        }
+        else if (FoodCount < 36)
+        {
+            pic5.SetActive(false);
+        }
+        else if (FoodCount < 43)
+        {
+            pic4.SetActive(false);
+        }
+        else if (FoodCount < 50)
+        {
             pic3.SetActive(false);
         }
-        else if (FoodCount<20)
+        else if (FoodCount < 57)
         {
             pic2.SetActive(false);
         }
-        else if (FoodCount<40)
+        else if (FoodCount < 64)
         {
             pic1.SetActive(false);
         }
--- Assets/Script/PlayerGen.cs
+++ Assets/Script/PlayerGen.cs
@@ -7,17 +7,22 @@ public class PlayerGen : MonoBehaviour
     
     void Start()
     {
-        Player one =  Instantiate(Resources.Load<Player>("Player"));
+        Player one =  Instantiate(Resources.Load<Player>("Player1"));
         one.SetPlayer(1);
-        Player two = Instantiate(Resources.Load<Player>("Player"));
+        Player two = Instantiate(Resources.Load<Player>("Player2"));
         two.SetPlayer(2);
-        Player three = Instantiate(Resources.Load<Player>("Player"));
+        Player three = Instantiate(Resources.Load<Player>("Player3"));
         three.SetPlayer(3);
-        Player four = Instantiate(Resources.Load<Player>("Player"));
+        Player four = Instantiate(Resources.Load<Player>("Player4"));
         four.SetPlayer(4);
-        Food Ham1 = Instantiate(Resources.Load<Food>("Bigmac"));
-        Ham1.SetID(5);
-
+        Food Set1 = Instantiate(Resources.Load<Food>("Foods"));
+        Set1.SetID(5);
+        Food Set2 = Instantiate(Resources.Load<Food>("Foods1"));
+        Set2.SetID(9);
+        Food Set3 = Instantiate(Resources.Load<Food>("Foods2"));
+        Set3.SetID(6);
+        Food Set4 = Instantiate(Resources.Load<Food>("Foods3"));
+        Set4.SetID(10);
 
     }
 
