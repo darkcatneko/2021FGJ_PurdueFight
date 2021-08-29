385b36e650bd36749cf95c3141b0ad5c87890472
Idleman <wnt2009@gmail.com>
2021-08-29 14:48:44 +0800

    Add Scene to build setting. Add LoadScene script. Add Tutorial tip in Main Menu scene. Main Menu、Select Character、Ghost Win、Human Win add load scene flow.

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    #region Behaviour'
+
+    public void Load(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    #endregion
+}
